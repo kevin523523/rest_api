5da6059e8e5c78029dab84ab2614b1b411dda210
kevin523523 <keviprad@espol.edu.ec>
2024-01-14 11:59:36 -0500

    update guia 22

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+/* MÓDULO dotenv */
+const dotenv = require('dotenv');
+/* CARGA DE DATOS DE CONFIGURACION EN MEMORIA */
+dotenv.config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var suppliersRouter = require('./routes/suppliers');
+
+/* Referencia al middleware */
+var authJWT = require('./middleware/auth');
 /* REFERENCIA AL MÓDULO */
 const swaggerUi = require('swagger-ui-express')
 
@@ -23,11 +30,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/users', usersRouter);
+/* Agregue el middleware para la ruta '/suppliers' */
+app.use('/suppliers', authJWT, suppliersRouter);
 /* CONFIGURACIÓN DE LA RUTA A LA DOCUMENTACIÓN */
 app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-app.use('/users', usersRouter);
-app.use('/suppliers', suppliersRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/suppliers.js
+++ routes/suppliers.js
@@ -5,6 +5,14 @@ const { Sequelize, Op } = require('sequelize');
 const Suppliers = require('../models').suppliers;
 
 router.get('/findAll', function (req, res, next) {
+      /* Verificador de autorización */
+
+  const { role } = req.user;
+
+  if (role !== process.env.ADMIN) {
+      return res.sendStatus(401);
+  }
+
     Suppliers.findAll({
     })
         .then(data => {
